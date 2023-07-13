ec85d778c9953fdd293625996f8100f2ed66bf34
Mert Turgay <76986528+MertTurgay@users.noreply.github.com>
2023-07-13 22:52:08 +0300

    Add files via upload

---
+++ spotify_api_vue/spotify_vue_app/src/App.vue
@@ -0,0 +1,125 @@
+<template>
+<div class="container-fluid">
+    <div class="header">
+      <img class="logo" src="./assets/spotify.png" alt="Spotify Logo" />
+      <h2 class="title">Search Popular Tracks from a Genre</h2>
+    </div>
+    <div class="search-bar mx-auto">
+      <div class="input-group">
+        <input type="text" class="form-control" v-model="genre" placeholder="Search a genre">
+        <div class="input-group-append">
+          <button class="btn btn-light" type="button" @click="searchTopTracks">
+            <img src="./assets/search2.png" alt="" width="24" height="24">
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="tracks-container">
+      <Track v-for="track in tracks" :key="track.track_name" :track="track" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Track from './components/Track.vue';
+
+export default {
+  components: {
+    Track
+  },
+  data() {
+    return {
+      genre: '',
+      tracks: [],
+    };
+  },
+  methods: {
+    searchTopTracks() {
+    axios.get(`http://127.0.0.1:8000/api/v1/tracks/${encodeURIComponent(this.genre)}/`)
+      .then(response => {
+        const tracks = response.data;
+        console.log('Track List:', tracks);
+        this.tracks = response.data;
+        
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background: #aef3e7;
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#5bc0be), color-stop(100%,#37718e));
+  background: -webkit-linear-gradient(top,  #aef3e7 0%, #37718e 100%);
+  background:    -moz-linear-gradient(top,  #aef3e7 0%, #37718e 100%);
+  background:     -ms-linear-gradient(top,  #aef3e7 0%, #37718e 100%);
+  background:      -o-linear-gradient(top,  #aef3e7 0%, #37718e 100%);
+  background:         linear-gradient(to bottom, #aef3e7 0%, #37718e 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#aef3e7', endColorstr='#37718e',GradientType=0 );
+  background-attachment: fixed;
+}
+
+.container-fluid {
+  padding: 20px;
+  color: #ffffff;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.logo {
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+}
+
+.title {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.search-bar {
+  margin-bottom: 20px;
+  width: 300px;
+
+}
+
+
+.tracks-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.input-group {
+  width: 300px;
+}
+
+.input-group-append {
+  position: relative;
+}
+
+.input-group-append .btn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-control {
+  height: 40px;
+  font-size: 16px;
+}
+
+
+
+</style>
---
+++ spotify_api_vue/spotify_vue_app/src/components/Track.vue
@@ -0,0 +1,79 @@
+
+<template>
+  <div class="track-component">
+    <div class="image-container">
+      <img :src="track.album_image" alt="Album Image" class="album-image">
+    </div>
+    <div class="track-info">
+      <h3 class="track-name">{{ track.track_name }}</h3>
+      <p class="artist-name">{{ track.artist_name }}</p>
+    </div>
+    <div class="audio">
+        <audio controls>
+            <source :src="track.preview_url" type="audio/mpeg" />
+        </audio>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    track: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.track-component {
+  display: flex;
+  align-items: center;
+  background-color: #254e70;
+  height: 100px;
+  width: 1000px;
+  margin-bottom: 10px;
+  border: 2px solid #37718e;
+  border-radius: 8px;
+}
+
+.image-container {
+  margin: 0 30px;
+}
+
+.album-image {
+  width: 96px;
+  height: 96px;
+}
+
+.track-info {
+  flex-grow: 1;
+}
+
+.track-name {
+  font-weight: bold;
+  font-size: 24px;
+  margin-bottom: 4px;
+  color: #ffffff;
+}
+
+.artist-name {
+  font-size: 18px;
+  color: #ffffff;
+}
+.audio{
+    margin-right: 20px;
+}
+.play-button {
+  background-color: transparent;
+  border: none;
+  width: 30px;
+  height: 30px;
+  margin-right: 20px;
+  background-image: url('../assets/play-button.png'); 
+  background-size: cover;
+  cursor: pointer;
+}
+</style>
